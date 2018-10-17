f15d4f71e4002cf9a83880cc3a6432977088533c
fadilnatakusumah <fadil.ntksmh@gmail.com>
2018-10-17 07:14:47 +0700

    fix database for heroku

--- config/database.php
+++ config/database.php
@@ -20,7 +20,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pqsql_heroku'),
+    'default' => env('DB_CONNECTION', 'pgsql_heroku'),
 
     /*
     |--------------------------------------------------------------------------
